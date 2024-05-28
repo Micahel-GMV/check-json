c1e6883ed96e2878d47dd13fde0c1ccad5caced1
Micahel <mvgudz@gmail.com>
2024-05-28 15:36:38 +0300

    Write testcases interest point

--- src/main/java/org/micahel/App.java
+++ src/main/java/org/micahel/App.java
@@ -1,6 +1,7 @@
 package org.micahel;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.File;
@@ -8,10 +9,14 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 import java.nio.file.*;
 import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class App
 {
@@ -31,55 +36,98 @@ public class App
         return content;
     }
 
+    public static String getJsonFromString(String content) {
+        char[] contentChars = content.toCharArray();
+        int beginIndex = 0;
+        while (contentChars[beginIndex] != '{' && contentChars[beginIndex] != '[') {
+            beginIndex++;
+            if (beginIndex == contentChars.length) {
+                System.out.println("Can`t find opening brackets. Returning input.");
+                return content;
+            }
+        };
+        int endIndex = contentChars.length - 1;
+        while (contentChars[endIndex] != '}' && contentChars[endIndex] != ']') {
+            endIndex--;
+            if (endIndex == -1) {
+                System.out.println("Can`t find closing brackets. Returning input.");
+                return content;
+            }
+        };
+        return content.substring(beginIndex, endIndex+1);
+    }
+
     private static void copyFile(File file, File outputDir) throws IOException {
         Path destination = outputDir.toPath().resolve(file.getName()).normalize();
         Files.copy(file.toPath(), destination, StandardCopyOption.REPLACE_EXISTING);
     }
 
-    private static boolean assertJson(String content) {
+    private static JsonNode assertJson(String content) {
         try {
             ObjectMapper objectMapper = new ObjectMapper();
-            objectMapper.readTree(content); // Will throw JsonProcessingException if content is not valid JSON
-            return true;
-        } catch (JsonProcessingException e) {
-            return false;
+            return objectMapper.readTree(content);
+        } catch (Exception e) {
+            System.out.println("JSON parsing error:" + e.getMessage());
+            return null;
         }
     }
 
-    private static boolean assertContains(String content) {
-        List<String> stringsToCheck = List.of("200",
-                                                "400",
-                                                "/greet",
-                                                "preconditions",
-                                                "missing or invalid parameter: name",
-                                                "actions",
-                                                "result");
-
-        for (String str : stringsToCheck) {
+    private static List<String> assertContains(String content, String...strings) {
+        List<String> notFound = new ArrayList<>();
+        for (String str : strings) {
             if (!content.toLowerCase().contains(str)) {
-                System.out.println("String " + str + " not found.");
-                return false;
+                notFound.add(str);
             }
         }
-        return true;
+        return notFound;
     }
 
-    public static boolean assertLinesCount(String content, int minLinesCount, int maxLinesCount) {
-        String[] lines = content.split("\r\n|\r|\n");
-        if (lines.length >= minLinesCount && lines.length <= maxLinesCount) {
-            return true;
+    public static boolean assertAtLeastOne(String content, String...strings) {
+        return assertContains(content, strings).size() < strings.length;
+    }
+
+    public static int linesCount(String content) {
+        return content.split("\r\n|\r|\n").length;
+    }
+
+    public static int assertLinesCount(String content, int minLinesCount, int maxLinesCount) {
+        int length = linesCount(content);
+        if (length >= minLinesCount && length <= maxLinesCount) {
+            return 0;
         }
-        System.out.println("Content lines count " + lines.length + " is out of boundaries from " + minLinesCount
-                + " to " + maxLinesCount + ".");
-        return false;
+        return length > maxLinesCount ? length - maxLinesCount : length - minLinesCount;
     }
 
-    public static void main( String[] args )
-    {
+    private static boolean askUserToAccept() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Do you want to accept this file? (y/n)");
+        String response = scanner.nextLine().trim().toLowerCase();
+        return response.equals("y");
+    }
+
+    public static String getListAsString (List<String> strings) {
+        return String.join(",", strings);
+    }
+
+    public static void main( String[] args ) throws IOException {
         Path inputPath = Paths.get(INPUT_FOLDER).toAbsolutePath().normalize();
         Path outputPath = Paths.get(OUTPUT_FOLDER).toAbsolutePath().normalize();
 
         File inputDir = inputPath.toFile(), outputDir = outputPath.toFile();
+        List<String> mandatoryStrings = List.of(
+                "200",
+                "400",
+                "/greet",
+                "precondition",
+                "missing or invalid parameter: name",
+                "action",
+                "result",
+                "localhost:8085",
+                "character");
+
+        List<String> oneOfCornercase = List.of(
+               "character", "edge", "corner");
+
 
         if (!outputDir.exists()) {
             outputDir.mkdirs();
@@ -88,20 +136,66 @@ public class App
         File[] files = inputDir.listFiles((dir, name) -> name.endsWith(".txt"));
 
         if (files != null) {
-            Arrays.stream(files).forEach(file -> {
+            int inputFilesCount = files.length;
+            System.out.println(inputFilesCount + " files to check.");
+            int fileIndex = 1;
+            for (File file : files) {
                 String content = readFile(file);
-                if (assertLinesCount(content, 30, 150) && assertContains(content) && assertJson(content)) {
-                    try {
-                        copyFile(file, outputDir);
-                        System.out.println(file.getName() + " copied.");
-                    } catch (IOException e) {
-                        System.out.println("Can`t copy the file " + file.getAbsolutePath() + " to " + outputDir.getAbsolutePath());
-                        throw new RuntimeException(e);
-                    }
+                content = getJsonFromString(content);
+                System.out.println("Input file: " + file.getName() + " ********************************************** ");
+                System.out.println(content);
+                System.out.println(" ********************************************** \n Checking the file "
+                        + fileIndex++ + " of " + inputFilesCount + ": " + file.getName());
+
+                boolean fileIsOk = true;
+
+                System.out.print("JSON mapping: ");
+                JsonNode jsonNode = assertJson(content);
+                if (jsonNode != null) {
+                    System.out.println("OK. Size:" + jsonNode.size());
                 } else {
-                    System.out.println(file.getName() + " didn`t pass the JSON mapping or didn`t contain mandatory string(s).");
+                    System.out.println("NOK. *****");
+                    fileIsOk = false;
                 }
-            });
+
+                System.out.print("Line count: " + linesCount(content) + ":");
+                int linesNumberProblems = assertLinesCount(content, 100, 500);
+                if (linesNumberProblems == 0) {
+                    System.out.println("OK.");
+                } else {
+                    System.out.println("NOK. *****: " + linesNumberProblems);
+                    fileIsOk = false;
+                }
+
+                System.out.print("Mandatory strings: ");
+                List<String> stringsNotFound = assertContains(content, mandatoryStrings.toArray(new String[0]));
+                if (stringsNotFound.isEmpty()) {
+                    System.out.println("OK");
+                } else {
+                    System.out.println("NOK. *****: " + getListAsString(stringsNotFound));
+                    fileIsOk = false;
+                }
+
+                System.out.print("Must be present one of the strings " + getListAsString(oneOfCornercase) + ":");
+                if (assertAtLeastOne(content, oneOfCornercase.toArray(oneOfCornercase.toArray(new String[0])))) {
+                    System.out.println("OK");
+                } else {
+                    System.out.println("NOK! None of strings found.");
+                    fileIsOk = false;
+                }
+
+                if (fileIsOk) {
+                    System.out.println("***** File is OK!");
+                } else {
+                    System.out.println("***** File has errors!");
+                }
+                if (fileIsOk && askUserToAccept()) {
+                    copyFile(file, outputDir);
+                    System.out.println("File " + file.getName() + " copied to output directory!");
+                } else {
+                    System.out.println("File " + file.getName() + " copying declined!");
+                }
+            }
         } else {
             System.out.println("No txt files in the input folder.");
         }
